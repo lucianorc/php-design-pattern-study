6c3a40bad0094cef9d4ecb4562a430491e641cb6
Luciano Costa <lcosta_96@hotmail.com.br>
2019-02-13 18:19:56 -0300

    Creational/LazyInitialization created

---
+++ Creational/LazyInitialization/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('Burger.php');
+require_once('BurgerLazyLoader.php');
+
+$burger = BurgerLazyLoader::getBurger(true, true);
+echo "Burger with cheese and fries costs: $ ".$burger->getPrice();
+
+echo "\n";
+echo "Instances in lazy loader: ".BurgerLazyLoader::getBurgerCount();
+echo "\n";
+
+$burger = BurgerLazyLoader::getBurger(true, false);
+echo "Burger with cheese and no fries costs: $ ".$burger->getPrice();
+
+echo "\n";
+echo "Instances in lazy loader: ".BurgerLazyLoader::getBurgerCount();
+echo "\n";
