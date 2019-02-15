1cd5ec3b19c4114682bad9616dee71f410a5f9ca
Luciano Costa <lcosta_96@hotmail.com.br>
2019-02-15 18:57:33 -0300

    Creational/BuilderPattern created

---
+++ Creational/BuilderPattern/Pizza.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('PizzaBuilder.php');
+
+class Pizza
+{
+    private $size;
+    private $cheese;
+    private $pepperoni;
+    private $bacon;
+
+    public function __construct(PizzaBuilder $builder)
+    {
+        $this->size = $builder->size;
+        $this->cheese = $builder->cheese;
+        $this->pepperoni = $builder->pepperoni;
+        $this->bacon = $builder->bacon;
+    }
+
+    public function show(): String
+    {
+        $recipe = "$this->size inch pizza with the following toppings: ";
+        $recipe .= $this->cheese ? "cheese, " : "";
+        $recipe .= $this->pepperoni ? "pepperoni, " : "";
+        $recipe .= $this->bacon ? "bacon, " : "";
+
+        return $recipe;
+    }
+}
